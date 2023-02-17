10a4e79c70b11d045f9bf4dfd0a6f1bb2963a96b
Victoria Cruz <yvictoriacs@hotmail.com>
2023-02-16 23:47:04 -0600

    eliminar

--- trabajador/urls.py
+++ trabajador/urls.py
@@ -4,5 +4,5 @@ urlpatterns = [
     path('create',views.create,name='createtrabajdor'),
     path ('consulta', views.consulta, name='consultatrabajador'),
     path ('editar', views.create, name='editartrabajador'),
-    path ('eliminar', views.create, name='eliminartrabajador')
+    path ('eliminar/<int:eliminacion>', views.eliminar, name='eliminartrabajador')
 ]
--- trabajador/views.py
+++ trabajador/views.py
@@ -1,30 +1,38 @@
-from django.shortcuts import render,redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from trabajador.forms import FormTrabajador
 from trabajador.models import Trabajador
 
 # Create your views here.
+
+
 def create(request):
-    if request.method=='POST':
-        form = FormTrabajador(request.POST) 
+    if request.method == 'POST':
+        form = FormTrabajador(request.POST)
         if form.is_valid():
             form.save()
             return redirect('consultatrabajador')
         else:
-            form=FormTrabajador(request.POST) 
-    
+            form = FormTrabajador(request.POST)
+
     else:
-        form= FormTrabajador()
-    return render(request,'agregartrabajador.html',{'form': form} )
+        form = FormTrabajador()
+    return render(request, 'agregartrabajador.html', {'form': form})
+
 
 def consulta(request):
     # dato= get_object_or_404(Trabajador)
-    dato= Trabajador.objects.all()
-    return render(request,'consulta.html',{'datos': dato} )
-def editar(request):
-    form= FormTrabajador()
-    return render(request,'editar.html',{'form': form} )
-def eliminar(request):
-    form= FormTrabajador()
-    return render(request,'eliminar.html',{'form': form} )
+    dato = Trabajador.objects.all()
+    return render(request, 'consulta.html', {'datos': dato})
 
+
+def editar(request):
+    form = FormTrabajador()
+    return render(request, 'editar.html', {'form': form})
+
+
+def eliminar(request, eliminacion):
+    trabajador_eliminar = get_object_or_404(Trabajador, pk=eliminacion)
+    if request.method == "POST":
+        trabajador_eliminar.delete()
+        return redirect("consultatrabajador")
