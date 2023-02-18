c24c640995e41e8171827804e5de08e07fa1af08
Victoria Cruz <yvictoriacs@hotmail.com>
2023-02-17 23:00:13 -0600

    XD

--- trabajador/forms.py
+++ trabajador/forms.py
@@ -1,12 +1,11 @@
 from django.forms import ModelForm, widgets,DateInput
-
 from trabajador.models import Trabajador
 
 class FormTrabajador(ModelForm):
     class Meta:
         model = Trabajador
         fields ='__all__'
-        widgets={
+        widgets={ 
             'fecha_de_nacimiento': DateInput(attrs={'type':'date'}),
             'fecha_contrato': DateInput(attrs={'type':'date'}),
             'fecha_finalizacion': DateInput(attrs={'type':'date'})
